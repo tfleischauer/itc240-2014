860a440262490e50867a400355ecde404b8bd43b
Troy Fleischauer <tfleischauer@gmail.com>
2014-04-30 14:04:47 -0700

    Array of Sunshine

---
+++ assignment-03/alph.php
@@ -0,0 +1,12 @@
+<li> 
+	<?= $album["title"]; ?>
+</li>
+
+<!--<li>
+ <?php /*?><?php 
+      ksort($albums);
+      foreach ($albums as $title => $value) {
+          echo "$value\n";
+      }
+  ?><?php */?>
+</li>-->
