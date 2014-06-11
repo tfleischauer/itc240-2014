7b045161e6a2fc870c80a38bcce8bf7ed08422c6
Troy Fleischauer <tfleischauer@gmail.com>
2014-06-11 03:47:50 -0700

    Added Sorting by Title or Author and CSS Theme option

--- assignment-07/index.php
+++ assignment-07/index.php
@@ -1,85 +1,6 @@
 <?php
 	include("passwords.php");
-?>
-
-<!doctype html>
-<html>
-	<head>
-    	<title>A7: Books</title>
-    	<style>
-			body {
-				font-family:"Century Gothic", "Trebuchet MS",  Helvetica, Arial, sans-serif;
-			}
-			
-			img {
-				width:150px;
-			}
-			
-			tr,
-			td {
-				border:2px solid lightgrey;
-				padding:15px;
-				border-collapse:collapse;	
-			}
-			
-			th {
-				text-align:center;
-				background-color:grey;
-				color:#FFF;	
-				padding:10px;
-				font-size:24px;
-			}
-			
-			table {
-				border-spacing:5px;	
-				text-align:left;
-				border-collapse:collapse;
-				margin:auto;	
-				margin-bottom:20px;
-			}
-			
-			a {
-				text-decoration:none;
-				color:#FFF;
-				border-bottom:3px dotted lightgrey;
-				
-			}
-			
-			a:hover {
-				text-decoration:underline;	
-				border-bottom:none;
-			}
-			
-			#buttons {
-				width:200px;
-				text-align:center;
-				margin:auto;	
-			}
-			
-			.dark-theme {
-				color:#FFF;
-				background-color:grey;
-			}
-			
-			.light-theme {
-				color:#000;
-				background-color:#FFF;
-			}
-			
-			.dark-theme a {
-				color:#FFF;
-				background-color:grey;
-				border-bottom:none;
-			}
-			
-			.light-theme a {
-				color:#000;
-				border-bottom:none;
-			}
-		</style>
-    </head>
-	<body>
-<?php	
+	
 	// if the browser is storing a key of 'sort' as a cookie, get that value
 	if (isset($_COOKIE["sortKey"])) {
 		$sortValue = $_COOKIE["sortKey"];	
@@ -110,65 +31,96 @@
 		$themeValue = $_REQUEST["themeKey"];
 		setcookie("themeKey", $themeValue, time() + 60 * 10, "/");	
 	}
+?>
+
+<!doctype html>
+<html>
+	<head>
+    	<title>A7: Books</title>
+    	<link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+	<body class="<?= $themeValue ?>-theme">
+    <div class="theme-container">
+    
+    <div id="buttons">
+        <a href="?viewKey=cover">Cover View</a> |
+        <a href="?viewKey=listing">Listing View</a> |
+
+		<a href="?themeKey=light">Light Theme</a> |
+        <a href="?themeKey=dark">Dark Theme</a>
+    </div>	
+    
+<?php		
 
 	// use white list to allow / control column names (safe list)
-	$sortingWhiteList = [
+	$sortingWhiteList = array (
 		"author" => true,
 		"title" => true,
 		"light" => true,
-		"dark" => true
-	];
+		"dark" => true,
+		"cover" => true,
+		"listing" => true
+	);
 	
 	
 	// if the value to the $sortBy key is not in $sortingWhiteList 
 	// assign  a default value
 	if (!isset($sortingWhiteList[$sortValue])) {
-		$sortKey = "title";	
+		$sortValue = "title";
 	}
 	
-	$sortQuery = $mysqlConnection->prepare("SELECT * FROM books ORDER BY $sortValue DESC;");
+	if (!isset($sortingWhiteList[$themeValue])) {
+		$themeValue = "light";
+	}
+	
+	if (!isset($sortingWhiteList[$viewValue])) {
+		$viewValue = "cover";	
+	}
+	
+	$sortQuery = $mysqlConnection->prepare("SELECT * FROM books ORDER BY $sortValue ASC");
 	$sortQuery->execute();
-	$sorted = $sortQuery->get_result();
+	$sorted = $sortQuery->get_result();	
 	
-	$booksAll = ("SELECT * FROM books");
-	$prepared = $mysqlConnection->prepare($booksAll);
-	// no input, no bind
-	$prepared->execute();
-	$results = $prepared->get_result();
-	
-	?>
-
-<table>
-	<tr>
-    	<th>Cover</th>
-        <th><a href="?sortKey=title">Title</a></th>
-        <th><a href="?sortKey=author">Author</a></th>
-        <th>Description</th>
-    </tr>
-	
-<?php
-	foreach($results as $result) {
-?>
-    
-    <tr> <!-- do I need htmlentities for displaying from the database? -->
-    	<td><img src="<?= htmlentities($result["cover_art"]) ?>" /></td>
-        <td><?= htmlentities($result["title"]) ?></td>
-        <td><?= htmlentities($result["author"]) ?></td>
-        <td><?= htmlentities($result["description"]) ?></td>
-    </tr>
-
-<?php
+	// cover view is the same as listing but also includes a description
+	// cover view shows everything and is the default
+	// listing view is the special case (itr jsut has no descripition)
+	if ($viewKey->listing) {
+		$viewQuery = ("SELECT cover_art, title, author FROM books");
+		$prepared = $mysqlConnection->prepare($viewQuery);
+		// no input, no bind
+		$prepared->execute();
+		$viewResult = $prepared->get_result();
+	} else { // same as default view, which is the cover view
+		$sortQuery = $mysqlConnection->prepare("SELECT * FROM books ORDER BY $sortValue ASC");
+		$sortQuery->execute();
+		$sorted = $sortQuery->get_result();		
 	}
-	?>
-</table>   
-
-	<div id="buttons">
-    	<button class="dark-theme"><a href="?viewKey=cover">Cover View</a></button>
-    	<button class="dark-theme"><a href="?viewKey=listing">Listing View</a></button>
-        <hr>
-		<button class="light-theme"><a href="?themeKey=light">Light Theme</a></button>
-		<button class="dark-theme"><a href="?themeKey=dark">Dark Theme</a></button>
-    </div>	
 	
-</body>
+	?>
+
+    <table>
+        <tr>
+            <th>Cover</th>
+            <th><a href="?sortKey=title">Title</a></th>
+            <th><a href="?sortKey=author">Author</a></th>
+            <th>Description</th>
+        </tr>
+        
+    <?php
+        foreach($sorted as $row) {
+    ?>
+        
+        <tr> <!-- do I need htmlentities for displaying from the database? -->
+            <td><img src="<?= htmlentities($row["cover_art"]) ?>" /></td>
+            <td><?= htmlentities($row["title"]) ?></td>
+            <td><?= htmlentities($row["author"]) ?></td>
+            <td><?= htmlentities($row["description"]) ?></td>
+        </tr>
+    
+    <?php
+        }
+        ?>
+    </table>   
+    </div> <!-- end theme-container -->	
+    </body>
 </html>
