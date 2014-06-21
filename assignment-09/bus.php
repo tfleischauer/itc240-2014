6ae9b290a0a209f54b65be41187e10a94d72f717
Troy Fleischauer <tfleischauer@gmail.com>
2014-06-20 18:24:12 -0700

    A9 Broken

--- assignment-09/bus.php
+++ assignment-09/bus.php
@@ -1,43 +1,39 @@
 <?php 
 	class Bus {
-		public $armed = "false";
-		public $exploded = "false";
 		public $speed = 0;
-		
-		function armed($speed) {
-			if ($speed < 50) {
-				$this->armed = "true";	
-			}
-			echo $this->armed . " is the armed status"; 
-		}
-		
-		function exploded($speed) {
-			if (($speed < 50) || ($armed == "true")) {
-				$this->exploded = "true";	
-			}	
-			echo $this->exploded . " is the exploded status"; ?><br><?php		
-		}
+		public $armed = false;
+		public $exploded = false;		
 		
 		function setSpeed($speed) {
 			$this->speed = $speed;
-			if (($speed > 50) && ($armed = "false")) {
-				$this->armed = "true";
+			
+			if (($speed < 50) && ($armed = false)){	
+				$this->exploded = false;
 			}
+			
+			elseif (($speed > 50) && ($exploded = false)) {
+				$this->armed = true;
+			}
+			
+			elseif (($speed < 50) && ($armed = true)) {
+				$this->exploded = true;	
+			}	
 		}
 		
 		function getSpeed() {
 			$this->speed;
-			echo $this->speed . " mph is the speed";	
 		}
 		
 		function trigger() {
-			$this->exploded = "true";	
+			$this->exploded = true;	
 		}	
 		
-		function showResults() {
-			echo $this->speed . " mph is the speed"; ?><br><?php
-			echo $this->armed . " is the armed status"; ?><br><?php
-			echo $this->exploded . " is the exploded status"; ?><br><?php
+		function boolToString($input) {
+			if ($input) {
+				return "true";
+			} else {
+				return "false";
+			}
 		}
 	}
 ?>
--- assignment-09/index.php
+++ assignment-09/index.php
@@ -2,29 +2,7 @@
 include("passwords.php");
 $mysqlConnection = new mysqli("localhost", "tfleis02", $mysql_password, "tfleis02");
 include("bus.php");
-
-// Test 1: Speed is at 20 mph
-$bus1_Object = new Bus();
-$bus1_Object->setSpeed(20);
-
-// Test 2: Speed is at 55 mph
-$bus2_Object = new Bus();
-$bus2_Object->setSpeed(55);
-
-// Test 3: Speed is at 80 mph
-$bus3_Object = new Bus();
-$bus3_Object->setSpeed(80);
-
-// Test 4: Speed is at 30 mph
-$bus4_Object = new Bus();
-$bus4_Object->setSpeed(30);
-
-// Test 5: Manually set exploded to false,
-// check value, call trigger
-$bus5_Object = new Bus();
-$bus5_Object->exploded = "false";
 ?>
-
 
 <!doctype html>
 <html>
@@ -35,27 +13,64 @@ $bus5_Object->exploded = "false";
 
 <body>
 <h1>Pop Quiz, Hotshot</h1>
-<?php
 
-?><h3>Test 1</h3><?php
- echo $bus1_Object->showResults(); ?><br><?php
- 
-?><h3>Test 2</h3><?php
- echo $bus2_Object->showResults(); ?><br><?php
- 
-?><h3>Test 3</h3><?php
- echo $bus3_Object->showResults(); ?><br><?php
- 
-?><h3>Test 4</h3><?php
- echo $bus4_Object->getSpeed(); ?><br><?php
- echo $bus4_Object->armed(30); ?><br><?php
- echo $bus4_Object->exploded(30); ?><br><?php
- 
-?><h3>Test 5</h3><?php
- echo $bus5_Object->exploded . " is the exploded status"; ?><br><?php 
- $bus5_Object->trigger();
- echo $bus5_Object->exploded . " is the exploded status"; ?><br><?php
- 
+<?php
+$bus = new Bus();
 ?>
+<h3>Test 1</h3>
+<?php
+// Test 1: Speed is at 20 mph
+$bus->setSpeed(20);
+?>
+ 
+<?php echo $bus->speed . " mph is the speed"; ?><br>
+<?php echo $bus->armed . " is the armed status"; ?><br>
+<?php $var1 = $bus->exploded ?><br>
+<?php $var2 = boolToString($var1) ?>
+<?php echo $var2 . " is the exploded status"; ?>
+
+<?php
+// Test 2: Speed is at 55 mph
+$bus->setSpeed(55);
+?>
+<h3>Test 2</h3>
+
+<?php echo $bus->speed . " mph is the speed"; ?><br>
+<?php echo $bus->armed . " is the armed status"; ?><br>
+<?php echo $bus->exploded . " is the exploded statuss"; ?><br>
+
+<?php
+// Test 3: Speed is at 80 mph
+$bus->setSpeed(80);
+?>
+<h3>Test 3</h3>
+
+<?php echo $bus->speed. " mph is the speed"; ?><br>
+<?php echo $bus->armed . " is the armed status"; ?><br>
+<?php echo $bus->exploded . " is the exploded statuss"; ?><br>
+
+<?php
+// Test 4: Speed is at 30 mph
+$bus->setSpeed(30);
+?>
+<h3>Test 4</h3>
+<?php echo $bus->getSpeed(); ?><br>
+<?php echo $bus->setSpeed(30); ?>
+<?php echo $bus->armed . " is the armed status"; ?><br>
+<?php echo $bus->exploded . " is the exploded statuss"; ?><br>
+ 
+
+<?php
+// Test 5: Manually set exploded to false,
+// check value, call trigger
+$bus->exploded = "false";
+?>
+<h3>Test 5</h3>
+<?php echo $bus->exploded . " is the exploded status"; ?><br>
+<?php $bus->trigger(); ?>
+<?php echo $bus->exploded . " is the exploded status"; ?><br>
+
+
+
 </body>
 </html>
